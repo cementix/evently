c41ccd281276f466af9a83e6a969196d1c504335
cementix <bodakondrackij@gmail.com>
2024-03-03 15:20:50 +0200

    middleware fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,37 +57,37 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    // try {
-    //   const {
-    //     id,
-    //     email_addresses,
-    //     image_url,
-    //     first_name,
-    //     last_name,
-    //     username,
-    //   } = evt.data;
-    //   const user = {
-    //     clerkId: id,
-    //     email: email_addresses[0].email_address,
-    //     username: username!,
-    //     firstName: first_name,
-    //     lastName: last_name,
-    //     photo: image_url,
-    //   };
-    //   const newUser = await prisma.user.create({
-    //     data: user,
-    //   });
-    //   if (newUser) {
-    //     await clerkClient.users.updateUserMetadata(id, {
-    //       publicMetadata: {
-    //         userId: newUser.clerkId,
-    //       },
-    //     });
-    //   }
-    //   return NextResponse.json({ message: "OK", user: newUser });
-    // } catch (error) {
-    //   handleError(error);
-    // }
+    try {
+      const {
+        id,
+        email_addresses,
+        image_url,
+        first_name,
+        last_name,
+        username,
+      } = evt.data;
+      const user = {
+        clerkId: id,
+        email: email_addresses[0].email_address,
+        username: username!,
+        firstName: first_name,
+        lastName: last_name,
+        photo: image_url,
+      };
+      const newUser = await prisma.user.create({
+        data: user,
+      });
+      if (newUser) {
+        await clerkClient.users.updateUserMetadata(id, {
+          publicMetadata: {
+            userId: newUser.clerkId,
+          },
+        });
+      }
+      return NextResponse.json({ message: "OK", user: newUser });
+    } catch (error) {
+      handleError(error);
+    }
   }
 
   if (eventType === "user.updated") {
