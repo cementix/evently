0a9b166634d25b6955ab0f514d5801b2ee840747
cementix <bodakondrackij@gmail.com>
2024-03-03 13:11:06 +0200

    test route

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,40 +57,41 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    try {
-      const {
-        id,
-        email_addresses,
-        image_url,
-        first_name,
-        last_name,
-        username,
-      } = evt.data;
-      const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url,
-      };
+    // try {
+    //   const {
+    //     id,
+    //     email_addresses,
+    //     image_url,
+    //     first_name,
+    //     last_name,
+    //     username,
+    //   } = evt.data;
+    //   const user = {
+    //     clerkId: id,
+    //     email: email_addresses[0].email_address,
+    //     username: username!,
+    //     firstName: first_name,
+    //     lastName: last_name,
+    //     photo: image_url,
+    //   };
 
-      const newUser = await prisma.user.create({
-        data: user,
-      });
+    //   const newUser = await prisma.user.create({
+    //     data: user,
+    //   });
 
-      if (newUser) {
-        await clerkClient.users.updateUserMetadata(id, {
-          publicMetadata: {
-            userId: newUser.clerkId,
-          },
-        });
-      }
+    //   if (newUser) {
+    //     await clerkClient.users.updateUserMetadata(id, {
+    //       publicMetadata: {
+    //         userId: newUser.clerkId,
+    //       },
+    //     });
+    //   }
 
-      return NextResponse.json({ message: "OK", user: newUser });
-    } catch (error) {
-      handleError(error);
-    }
+    //   return NextResponse.json({ message: "OK", user: newUser });
+    // } catch (error) {
+    //   handleError(error);
+    // }
+    console.log("test");
   }
 
   if (eventType === "user.updated") {
